462f2c9826e81b19af8eeca0f16e8d355d199291
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2016-09-09 17:56:28 +0100

    adding a gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp  = require('gulp');
+var shell = require('gulp-shell');
+
+//  "deploy-gitbook": "./scripts/losh deploy-gitbook",
+gulp.task('deploy', function () {
+  return gulp.src('').pipe(shell(["./scripts/losh deploy-gitbook"]));
+});
+
+// npm install -g http-server
+//  "generate-gitbook": "./scripts/generate-gitbook",
+gulp.task('build', function() {
+  return gulp.src('').pipe(shell(['./scripts/generate-gitbook']));
+});
+
--- txt/README.md
+++ txt/README.md
@@ -4,7 +4,9 @@
 
 ## Apuntes
 
-[apuntes!](http://crguezl.github.io/ull-esit-1617)
+* [apuntes!](http://crguezl.github.io/ull-esit-1617)
+* [Apuntes del Curso 16/17 en gitbook.com](https://www.gitbook.com/book/casianorodriguezleon/ull-esit-1617/details)
+* [Wiki del curso 16/17 en GitHub](https://github.com/crguezl/ull-esit-1617/wiki)
 
 ## Background
 
