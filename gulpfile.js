8b84f5962c69de9d6a16ea4922a933b59bca5e27
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2016-09-18 21:35:01 +0100

    tasks

--- gulpfile.js
+++ gulpfile.js
@@ -16,3 +16,14 @@ gulp.task('build', function() {
 gulp.task('serve', function() {
   return gulp.src('').pipe(shell(['gitbook serve txt gh-pages']));
 });
+
+
+// open browser at https://crguezl.github.io/ull-esit-1617/
+gulp.task('opengh', function() {
+  return gulp.src('').pipe(shell(['open https://crguezl.github.io/ull-esit-1617/']));
+});
+
+// open browser at https://casianorodriguezleon.gitbooks.io/ull-esit-1617/content/
+gulp.task('open', function() {
+  return gulp.src('').pipe(shell(['open https://casianorodriguezleon.gitbooks.io/ull-esit-1617/content/']));
+});
