19dc43618905a1184ebb9a683640583eb2fc4a11
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-03-23 19:03:26 +0000

    deploy to gitbooks

--- gulpfile.js
+++ gulpfile.js
@@ -59,6 +59,8 @@ gulp.task('build', function() {
   return gulp.src('').pipe(shell(['./scripts/generate-gitbook']));
 });
 
+gulp.task('build2', shell.task(['gitbook build']));
+
 // "serve": "gitbook serve txt gh-pages",
 gulp.task('serve', function() {
   return gulp.src('').pipe(shell(['gitbook serve --lrport 99990 --port 43210 `pwd` gh-pages']));
